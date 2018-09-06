c8bf7296ccf3b4b6c9d8a4e2806ad73f9183f796
madihaparacha <38943126+madihaparacha@users.noreply.github.com>
2018-09-06 16:43:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function onloadFunc(){
 	for (let i=0; i<10; i++) {
 		document.querySelector(".main-div").innerHTML += 
 		`<div class='news-div'>
+                <a href='${data.articles[i].url}'>
 		<div class='image-div'>
 		<img src='${data.articles[i].urlToImage}'>
-                <a href='${data.articles[i].url}'>
         <h1 class="news-title">${data.articles[i].title}</h1>
 		</div>
 		<p class='news-para'>${data.articles[i].description}</p>
