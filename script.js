88a3b2ad9477a454f494ab0a826124791d511de0
madihaparacha <38943126+madihaparacha@users.noreply.github.com>
2018-09-06 16:35:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function onloadFunc(){
 		document.querySelector(".main-div").innerHTML += 
 		`<div class='news-div'>
 		<div class='image-div'>
-                <a href='${data.articles[i].urlToImage}'>
+                <a href='${data.articles[i].url}'>
 		<img src='${data.articles[i].urlToImage}'>
         <h1 class="news-title">${data.articles[i].title}</h1>
 		</div>
